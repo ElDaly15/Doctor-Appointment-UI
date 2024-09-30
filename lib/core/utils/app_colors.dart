e92385deff3f53c23160d64a37a20fe6c6e51d50
Daly <monokakoman@gmail.com>
2024-09-30 09:16:44 +0300

    first commit

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff4894FE);
+  static const Color backgroundColor = Color(0xffF5F5F5);
+  static const Color textColor = Color(0xff8696BB);
+  static const Color textSecondaryTitles = Color(0xffF5F5F5);
+  static const Color textMainTitles = Color(0xff0D1B34);
+}
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.svg
+  static const String imagesAvatar = "assets/images/avatar.svg";
+
+  /// Assets for imagesCalendar
+  /// assets/images/calendar.svg
+  static const String imagesCalendar = "assets/images/calendar.svg";
+
+  /// Assets for imagesDoctor1
+  /// assets/images/doctor1.svg
+  static const String imagesDoctor1 = "assets/images/doctor1.svg";
+
+  /// Assets for imagesDoctor2
+  /// assets/images/doctor2.svg
+  static const String imagesDoctor2 = "assets/images/doctor2.svg";
+
+  /// Assets for imagesDoctor3
+  /// assets/images/doctor3.svg
+  static const String imagesDoctor3 = "assets/images/doctor3.svg";
+
+  /// Assets for imagesDoctor4
+  /// assets/images/doctor4.svg
+  static const String imagesDoctor4 = "assets/images/doctor4.svg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesHospital
+  /// assets/images/hospital.svg
+  static const String imagesHospital = "assets/images/hospital.svg";
+
+  /// Assets for imagesLink
+  /// assets/images/link.svg
+  static const String imagesLink = "assets/images/link.svg";
+
+  /// Assets for imagesMessage
+  /// assets/images/message.svg
+  static const String imagesMessage = "assets/images/message.svg";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.svg
+  static const String imagesProfile = "assets/images/profile.svg";
+
+  /// Assets for imagesProfileAdd
+  /// assets/images/profile-add.svg
+  static const String imagesProfileAdd = "assets/images/profile-add.svg";
+
+  /// Assets for imagesSun
+  /// assets/images/sun.svg
+  static const String imagesSun = "assets/images/sun.svg";
+}
